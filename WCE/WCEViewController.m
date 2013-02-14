3081c1f04efb8f17cb574d1842144f682b5f3679
Peter <peter@137-28-41-155-wireles1x.bu.edu>
2013-02-13 19:54:37 -0500

    Made a change

--- WCE/WCEViewController.m
+++ WCE/WCEViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+	[[self view] setBackgroundColor:[UIColor redColor]];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
