54d77167f2fa460f34f1a6b6004cb461951e8eba
Brian Beckerle <beckerlelumba1@aol.com>
2013-02-18 21:59:07 -0500

    Attempting to implement pickerView for the location view

--- LocationViewController.h
+++ LocationViewController.h
@@ -8,6 +8,17 @@
 
 #import <UIKit/UIKit.h>
 
-@interface LocationViewController : UIViewController
+@interface LocationViewController : UIViewController <UIPickerViewDelegate, UIPickerViewDataSource> {
+    IBOutlet UIPickerView *locationPicker;
+    NSMutableArray *locationArray;
+}
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView;
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component;
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component;
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component;
 
 @end
--- LocationViewController.m
+++ LocationViewController.m
@@ -19,6 +19,7 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
+        [locationPicker setDelegate: self];
     }
     return self;
 }
@@ -27,12 +28,36 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    locationArray = [[NSMutableArray alloc] init];
+    [locationArray addObject:@"Liberia"];
+    [locationArray addObject:@"Pakistan"];
+    [locationArray addObject:@"Stony Point"];
+    
+    
 }
+
+
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+    return [locationArray count];
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
+    return [locationArray objectAtIndex:row];
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+    NSLog(@"Selected Location: %@. Index of selected color: %i", [locationArray objectAtIndex:row], row);
 }
 
 @end
