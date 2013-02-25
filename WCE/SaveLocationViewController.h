e29b09906b0a6134af321851b00fed5366a47e75
Brian Beckerle <beckerlelumba1@aol.com>
2013-02-25 18:31:04 -0500

    got rid of extra navbar in storyboard and made a SaveLocationViewController

---
+++ WCE/SaveLocationViewController.h
@@ -0,0 +1,13 @@
+//
+//  SaveLocationViewController.h
+//  WCE
+//
+//  Created by  Brian Beckerle on 2/25/13.
+//  Copyright (c) 2013  Brian Beckerle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SaveLocationViewController : UIViewController
+
+@end
