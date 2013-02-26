9c8585df6c3b50744d0a56a11d07e3f216e91aac
Sushruth Chandrasekar <sushruth@bu.edu>
2013-02-26 12:45:22 -0500

    I am coding the map thing and I got 1 stinking error!! Someone please help!! Once this error is fixed the map should be working

--- WCE/LocationMapViewController.h
+++ WCE/LocationMapViewController.h
@@ -5,7 +5,9 @@
 //
 
 #import <UIKit/UIKit.h>
-
+#import <MapKit/MapKit.h>
+#import <ArcGIS/ArcGIS.h>
 @interface LocationMapViewController : UIViewController
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
 
-@end
+@end	
--- WCE/LocationMapViewController.m
+++ WCE/LocationMapViewController.m
@@ -6,38 +6,39 @@
 
 #import "LocationMapViewController.h"
 
-@interface LocationMapViewController ()
+@interface ViewController: UIViewController <AGSMapViewLayerDelegate>
 
 @end
 
 @implementation LocationMapViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
-}
-
--(void)viewWillAppear:(BOOL)animated{
-    //hide navigation bar, one is shown by storyboard
-    [self.navigationController setNavigationBarHidden:YES];
     
+    AGSTiledMapServiceLayer *tiledLayer =
+    [AGSTiledMapServiceLayer
+     tiledMapServiceLayerWithURL:[NSURL URLWithString:@"http://server.arcgisonline.com/ArcGIS/rest/services/ESRI_StreetMap_World_2D/MapServer"]];
+    [self.mapView addMapLayer:tiledLayer withName:@"Tiled Layer"];
     
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)mapView:(AGSMapView *) mapView failedLoadingLayerForLayerView:(UIView *) layerView baseLayer:(BOOL) baseLayer withError:(NSError *) error {
+    
+    //Inspect the error and then either ...
+    //A. Remove the layer if it's not essential
+    if(!baseLayer)
+        [self.mapView removeMapLayerWithName:layerView.name];
+    
+    //B. Or, try resubmitting the layer, with different URL or credentials perhaps
+    AGSTiledMapServiceLayer* tiledLyr = (AGSTiledMapServiceLayer*)laverView.agsLayer;
+    [tiledLyr resubmitWithURL:url credential:cred];
+    
 }
-
+- (void)viewDidLoad {
+    ...
+    
+    self.mapView.layerDelegate = self;
+    
+}
 @end
