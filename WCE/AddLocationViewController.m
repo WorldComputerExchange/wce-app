cc5ac7876d60fbb04f1984f8fe6a95d04bdbb259
Brian Beckerle <beckerlelumba1@aol.com>
2013-02-25 20:08:26 -0500

    Added View Controller code for Add Location Screen

---
+++ WCE/AddLocationViewController.m
@@ -0,0 +1,38 @@
+//
+//  AddLocationViewController.m
+//  WCE
+//
+//  Created by  Brian Beckerle on 2/25/13.
+//  Copyright (c) 2013  Brian Beckerle. All rights reserved.
+//
+
+#import "AddLocationViewController.h"
+
+@interface AddLocationViewController ()
+
+@end
+
+@implementation AddLocationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
