58793137af6bafab5f986ead061fdd380ab049ed
Sushruth Chandrasekar <sushruth@bu.edu>
2013-02-26 02:32:25 -0500

    Added picker view for AddLocationViewController, not totally working yet, also cleaned up storyboard a tiny bit

--- WCE/AddLocationViewController.h
+++ WCE/AddLocationViewController.h
@@ -6,6 +6,13 @@
 
 #import <UIKit/UIKit.h>
 
-@interface AddLocationViewController : UIViewController
+@interface AddLocationViewController : UIViewController <UIPickerViewDelegate, UIPickerViewDataSource>
+{
+    NSArray *locations;
+    UIActionSheet *actionSheet;
+}
+
+@property (nonatomic, copy) NSArray *locations;
+@property (nonatomic, copy) UIActionSheet *actionSheet;
 
 @end
--- WCE/AddLocationViewController.m
+++ WCE/AddLocationViewController.m
@@ -12,11 +12,12 @@
 
 @implementation AddLocationViewController
 
+@synthesize locations, actionSheet;
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
     }
     return self;
 }
@@ -26,13 +27,60 @@
     //make navigation bar hidden bc it is put in in storyboard
     NSLog(@"IS this being called?");
     [self.navigationController setNavigationBarHidden:YES];
+    
+    actionSheet = [[UIActionSheet alloc] initWithTitle:nil
+                                              delegate:nil
+                                     cancelButtonTitle:nil
+                                destructiveButtonTitle:nil
+                                     otherButtonTitles:nil];
+    
+    [actionSheet setActionSheetStyle:UIActionSheetStyleBlackTranslucent];
+    
+    CGRect pickerFrame = CGRectMake(0, 40, 0, 0);
+    UIPickerView *pickerView = [[UIPickerView alloc] initWithFrame:pickerFrame];
+    pickerView.showsSelectionIndicator = YES;
+    pickerView.dataSource = self;
+    pickerView.delegate = self;
+    
+    UISegmentedControl *closeButton = [[UISegmentedControl alloc] initWithItems:[NSArray arrayWithObject:@"Done"]];
+    closeButton.momentary = YES;
+    closeButton.frame = CGRectMake(260, 7.0f, 50.0f, 30.0f);
+    closeButton.segmentedControlStyle = UISegmentedControlStyleBar;
+    closeButton.tintColor = [UIColor blueColor];
+    [closeButton addTarget:self action:@selector(pickerDoneClicked) forControlEvents:UIControlEventValueChanged];
+    
+    [actionSheet addSubview:pickerView];
+    [actionSheet addSubview:closeButton];
+    [actionSheet showInView:[[UIApplication sharedApplication] keyWindow]];
+    [actionSheet setBounds:CGRectMake(0, 0, 320, 485)];
 }
 
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return [locations count];
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    return [locations objectAtIndex:row];
+}
+
+- (void)pickerDoneClicked
+{
+    [actionSheet dismissWithClickedButtonIndex:0 animated:YES];
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    locations = [[NSArray alloc] initWithObjects:@"Libya", @"Boston", @"Chicago", @"Cairo", @"The Vatican", nil];
 }
 
 - (void)didReceiveMemoryWarning
