015a8a948c16174c289160ab3b11afc136c87db6
Sushruth Chandrasekar <sushruth@bu.edu>
2013-03-01 11:16:21 -0500

    In the process of creating the drop down menus for the add location screen

---
+++ WCE/DropDownMenu.h
@@ -0,0 +1,14 @@
+//
+//  DropDownMenu.h
+//  WCE
+//
+//  Created by Sushruth Chandrasekar on 3/1/13.
+//  Copyright (c) 2013  Brian Beckerle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DropDownMenu : UIViewController <**UItableViewDataSource, UITableViewDelegate**>
+
+
+@end
---
+++ WCE/DropDownMenu.m
@@ -0,0 +1,31 @@
+//
+//  DropDownMenu.m
+//  WCE
+//
+//  Created by Sushruth Chandrasekar on 3/1/13.
+//  Copyright (c) 2013  Brian Beckerle. All rights reserved.
+//
+
+#import "DropDownMenu.h"
+
+@implementation DropDownMenu
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
