5c1a2a5905a68f027347c824a3eed77b1c02a904
Brian Beckerle <beckerlelumba1@aol.com>
2013-03-02 12:40:53 -0500

    changed locations in location screen to regions of interest and added location object for the rest of the app to use

---
+++ WCE/Location.h
@@ -0,0 +1,15 @@
+//
+//  Location.h
+//  WCE
+//
+//  Created by  Brian Beckerle on 3/2/13.
+//  Copyright (c) 2013  Brian Beckerle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Location : NSObject
+
+
+
+@end
---
+++ WCE/Location.m
@@ -0,0 +1,13 @@
+//
+//  Location.m
+//  WCE
+//
+//  Created by  Brian Beckerle on 3/2/13.
+//  Copyright (c) 2013  Brian Beckerle. All rights reserved.
+//
+
+#import "Location.h"
+
+@implementation Location
+
+@end
