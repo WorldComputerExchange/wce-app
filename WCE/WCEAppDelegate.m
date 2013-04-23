384619b6607e5841c3e71111b1714f96ed816bf1
Sushruth Chandrasekar <sushruth@bu.edu>
2013-04-23 19:40:21 -0400

    check this out

--- WCE/WCEAppDelegate.m
+++ WCE/WCEAppDelegate.m
@@ -14,7 +14,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 
 {
-    self.window.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"NH Check Ceremonty Nashville_0.jpg"]];    // Override point for customization after application launch.
+       // Override point for customization after application launch.
     return YES;
     
     
@@ -24,30 +24,30 @@
 
 							
 - (void)applicationWillResignActive:(UIApplication *)application
-{self.window.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"NH Check Ceremonty Nashville_0.jpg"]];
+{
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
-{self.window.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"NH Check Ceremonty Nashville_0.jpg"]];
+{self.window.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"Kiang_West_savanna.jpg"]];
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
-{self.window.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"NH Check Ceremonty Nashville_0.jpg"]];
+{self.window.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"Kiang_West_savanna.jpg"]];
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
-{self.window.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"NH Check Ceremonty Nashville_0.jpg"]];
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+
+{self.window.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"Kiang_West_savanna.jpg"]];    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
-{
+{self.window.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"Kiang_West_savanna.jpg"]];
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
