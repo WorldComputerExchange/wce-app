b29afba0a7a930cc7457a3af94f8df927ea4c6e6
Alex <alex@Alexs-MacBook-Air-2.local>
2013-07-27 13:46:49 -0400

    Evaluation Form:  Text fields and views now property declared.  Scroll view now pushes up textfield when keyboard pops up.  Still need to figure out how to do this for TextView.

--- WCE/EvaluationViewController.m
+++ WCE/EvaluationViewController.m
@@ -9,10 +9,170 @@
 #import "EvaluationViewController.h"
 
 @interface EvaluationViewController ()
-
+@property (nonatomic, readonly) CGPoint originalOffset;
+@property (nonatomic, readonly) UIView *activeField;
 @end
 
 @implementation EvaluationViewController
+
+
+//@synthesize locations, countries, languages, actionSheet, dropDownTableView, dataArray, sharedUser, selectedCountry, selectedLanguage;
+
+@synthesize efq1, efq2, efq3, efq4, efq5, efq6, efq7, efq8, efq9, efq10, efq11, efq12, efq13, efq14, efq15, efq16, efq17, efq18, efq19, efq20, efq21, efq22, efq23, efq24, efq25, efq26, efq27, efq28;
+
+
+
+//FROM TUTORIAL Cocoa W/Love; need the following instance variables
+CGFloat animatedDistance;
+//FROM TUTORIAL Cocoa W/Love; need the following instance constants
+static const CGFloat KEYBOARD_ANIMATION_DURATION = 0.3;
+static const CGFloat MINIMUM_SCROLL_FRACTION = 0.2;
+static const CGFloat MAXIMUM_SCROLL_FRACTION = 0.8;
+static const CGFloat PORTRAIT_KEYBOARD_HEIGHT = 216;
+static const CGFloat LANDSCAPE_KEYBOARD_HEIGHT = 162;
+
+- (IBAction)backgroundTouched:(id)sender {
+    [efq1 resignFirstResponder];
+    [efq2 resignFirstResponder];
+    [efq3 resignFirstResponder];
+    [efq4 resignFirstResponder];
+    [efq5 resignFirstResponder];
+    [efq6 resignFirstResponder];
+    [efq7 resignFirstResponder];
+    [efq8 resignFirstResponder];
+    [efq9 resignFirstResponder];
+    [efq10 resignFirstResponder];
+    [efq11 resignFirstResponder];
+    [efq12 resignFirstResponder];
+    [efq13 resignFirstResponder];
+    [efq14 resignFirstResponder];
+    [efq15 resignFirstResponder];
+    [efq16 resignFirstResponder];
+    [efq17 resignFirstResponder];
+    [efq18 resignFirstResponder];
+    [efq19 resignFirstResponder];
+    [efq20 resignFirstResponder];
+    [efq21 resignFirstResponder];
+    [efq22 resignFirstResponder];
+    [efq23 resignFirstResponder];
+    [efq24 resignFirstResponder];
+    [efq25 resignFirstResponder];
+    [efq26 resignFirstResponder];
+    [efq27 resignFirstResponder];
+    [efq28 resignFirstResponder];
+}
+
+
+
+
+
+
+//FROM TUTORIAL: www.cocoawithlove.com/2008/10/sliding-uitextfields-around-to-avoid.html
+
+//Animate upwards when the text field is selected
+//Get the rects of the text field being edited and the view that we're going to scroll. We convert everything to window coordinates, since they're not necessarily in the same coordinate space.
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    CGRect textFieldRect =
+    [self.view.window convertRect:textField.bounds fromView:textField];
+    CGRect viewRect =
+    [self.view.window convertRect:self.view.bounds fromView:self.view];
+
+    //So now we have the bounds, we need to calculate the fraction between the top and bottom of the middle section for the text field's midline:
+    CGFloat midline = textFieldRect.origin.y + 0.5 * textFieldRect.size.height;
+    CGFloat numerator =
+    midline - viewRect.origin.y
+    - MINIMUM_SCROLL_FRACTION * viewRect.size.height;
+    CGFloat denominator =
+    (MAXIMUM_SCROLL_FRACTION - MINIMUM_SCROLL_FRACTION)
+    * viewRect.size.height;
+    CGFloat heightFraction = numerator / denominator;
+    
+    
+    //Clamp this fraction so that the top section is all "0.0" and the bottom section is all "1.0".
+    if (heightFraction < 0.0)
+    {
+        heightFraction = 0.0;
+    }
+    else if (heightFraction > 1.0)
+    {
+        heightFraction = 1.0;
+    }
+    
+    
+    //Now take this fraction and convert it into an amount to scroll by multiplying by the keyboard height for the current screen orientation. Notice the calls to floor so that we only scroll by whole pixel amounts.
+    UIInterfaceOrientation orientation =
+    [[UIApplication sharedApplication] statusBarOrientation];
+    if (orientation == UIInterfaceOrientationPortrait ||
+        orientation == UIInterfaceOrientationPortraitUpsideDown)
+    {
+        animatedDistance = floor(PORTRAIT_KEYBOARD_HEIGHT * heightFraction);
+    }
+    else
+    {
+        animatedDistance = floor(LANDSCAPE_KEYBOARD_HEIGHT * heightFraction);
+    }
+    
+    
+    //Finally, apply the animation. Note the use of setAnimationBeginsFromCurrentState: — this will allow a smooth transition to new text field if the user taps on another.
+    CGRect viewFrame = self.view.frame;
+    viewFrame.origin.y -= animatedDistance;
+    
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationBeginsFromCurrentState:YES];
+    [UIView setAnimationDuration:KEYBOARD_ANIMATION_DURATION];
+    
+    [self.view setFrame:viewFrame];
+    
+    [UIView commitAnimations];
+}
+
+
+//ANIMATE BACK AGAIN:  The return animation is far simpler since we've saved the amount to animate.
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    CGRect viewFrame = self.view.frame;
+    viewFrame.origin.y += animatedDistance;
+    
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationBeginsFromCurrentState:YES];
+    [UIView setAnimationDuration:KEYBOARD_ANIMATION_DURATION];
+    
+    [self.view setFrame:viewFrame];
+    
+    [UIView commitAnimations];
+}
+
+//dismisses the keyboard when the return/done button is pressed.
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -31,16 +191,203 @@
     return self;
 }
 
+
+
+
+
+
+//from stackoverflow.com/questions/7130982/keyboardwasshown-method-not-being-called-on-my-uitextview
+/**
+-(void) viewWillAppear: (BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    [[self view] setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"136676912132100.gif"]]];
+    
+    NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+    [nc addObserver:self selector:@selector(keyboardWasShown:) name: UIKeyboardWillShowNotification object:nil];
+    [nc addObserver:self selector:@selector(keyboardWasHidden:) name: UIKeyboardWillHideNotification object:nil];
+
+}*/
+- (void) viewWillDisappear: (BOOL)animated{
+    
+    [super viewWillDisappear:animated];
+    
+    NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+    [nc removeObserver:self name: UIKeyboardWillShowNotification object:nil];
+    [nc removeObserver:self name: UIKeyboardWillHideNotification object:nil];
+}
+
+
+
+
+
+- (void)viewWillAppear:(BOOL)animated
+{
+	[[self view] setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"136676912132100.gif"]]];
+
+}
+
+
+
+
+
+
+
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
+
+
+/*
+
+
+/// called when a text field is selected
+//- (void)textFieldDidBeginEditing:(UITextField *)textField
+//{
+	//_activeField = textField;
+//}
+
+
+
+//- (void)textFieldDidBeginEditing:(UITextField *)textField
+//{
+  //  [self animateTextField: textField up: YES];
+//}
+
+
+
+
+
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    [self animateTextField: textField up: YES];
+}
+
+- (void) animateTextField: (UITextField*) textField up: (BOOL) up
+{
+    const int movementDistance = 80; // tweak as needed
+    const float movementDuration = 0.3f; // tweak as needed
+    
+    int movement = (up ? -movementDistance : movementDistance);
+    
+    [UIView beginAnimations: @"anim" context: nil];
+    [UIView setAnimationBeginsFromCurrentState: YES];
+    [UIView setAnimationDuration: movementDuration];
+    self.view.frame = CGRectOffset(self.view.frame, 0, movement);
+    [UIView commitAnimations];
+}
+
+
+
+
+
+
+
+
+
+
+// Called when a button is clicked in the UIAlertView
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+	if(buttonIndex == 1)
+		[self dismissViewControllerAnimated:YES completion:nil];
+}
+
+// Called when a button is clicked in the UIActionSheet
+- (void)actionSheet:(UIActionSheet *)actionSheet willDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+    if(buttonIndex == 0)
+        [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+
+
+
+
+
+// called when the keyboard is shown
+- (void)keyboardWasShown:(NSNotification*)aNotification
+{
+    NSDictionary* info = [aNotification userInfo];
+    CGRect kbRect = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue];
+    kbRect = [self.view convertRect:kbRect toView:nil];
+    UIEdgeInsets contentInsets = UIEdgeInsetsMake(0.0, 0.0, kbRect.size.height, 0.0);
+    _scrollView.contentInset = contentInsets;
+    _scrollView.scrollIndicatorInsets = contentInsets;
+	
+    CGRect aRect = self.view.frame;
+    aRect.size.height -= kbRect.size.height;
+    //aRect.size.height -= self.toolbar.frame.size.height;
+    CGPoint fieldOrigin = _activeField.frame.origin;
+    fieldOrigin.y -= _scrollView.contentOffset.y;
+    fieldOrigin = [self.view convertPoint:fieldOrigin toView:self.view.superview];
+    _originalOffset = _scrollView.contentOffset;
+	
+	// scroll the scrollview down a tiny bit more
+	CGRect fieldRect = _activeField.frame;
+	CGRect tallerRect = CGRectMake(fieldRect.origin.x, fieldRect.origin.y, fieldRect.size.width, fieldRect.size.height + 50); // 50 more
+	
+    if (!CGRectContainsPoint(aRect, fieldOrigin) ) {
+        [_scrollView scrollRectToVisible:tallerRect animated:YES];
+    }
+}
+
+
+ 
+
+
+-- from stackoverflow.com/questions/7952762/xcode-ios5-move-uiview-up-when-keyboard-appears?answertab=active#tab-top
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:0.25];
+    self.view.frame = CGRectMake(0,-10,320,400);
+    [UIView commitAnimations];
+    
+}
+*/
+
+/*
+/// called when the keyboard is hidden
+- (void)keyboardWillBeHidden:(NSNotification*)aNotification {
+	UIEdgeInsets contentInsets = UIEdgeInsetsZero;
+	_scrollView.contentInset = contentInsets;
+	_scrollView.scrollIndicatorInsets = contentInsets;
+	[_scrollView setContentOffset:_originalOffset animated:YES];
+}
+*/
+
+
+
+
 
 @end
--- WCE/ImplementationViewController.h
+++ WCE/ImplementationViewController.h
@@ -6,13 +6,29 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ImplementationViewController : UIViewController
+
+@interface ImplementationViewController : UIViewController <UIActionSheetDelegate, UITextFieldDelegate, UITextViewDelegate>
 {
+    UIActionSheet *actionSheet;
+    
 }
 
-@property UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+//@property (nonatomic, copy) UIActionSheet *actionSheet;
 
+@property (weak, nonatomic) IBOutlet UITextField *iqq1;
+@property (weak, nonatomic) IBOutlet UITextField *iqq2;
+@property (weak, nonatomic) IBOutlet UITextView *iqq3;
+@property (weak, nonatomic) IBOutlet UITextView *iqq4;
+@property (weak, nonatomic) IBOutlet UITextView *iqq5;
+@property (weak, nonatomic) IBOutlet UITextField *iqq6;
 
+- (IBAction)backgroundTouched:(id)sender;
 - (IBAction)textfieldReturn:(id)sender;
+
+
+
+//- (IBAction)cancelChanges:(id)sender;
+//- (IBAction)saveChanges:(id)sender;
 
 @end
